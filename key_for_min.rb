0b6fa53d0ff531e32a78686fad9dbb5e95109735
Joshua Williams <jmw0426@hotmail.com>
2020-03-30 16:41:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
- name_hash.count
+ 
+ name_hash.count do {|x| x == 0}
+   puts "#{x}"
+   
 
 end

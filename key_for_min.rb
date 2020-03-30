250d69a7ee7236dd929e334c60ade59ff29c919c
Joshua Williams <jmw0426@hotmail.com>
2020-03-30 16:50:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if
+  if name_hash.length == 0
+   return nil
+    
+  else 
     !name_hash.max_by{|k,v| v}
    return "#{v}"
-  
-  else name_hash.length == 0
-   return nil
  end
  end

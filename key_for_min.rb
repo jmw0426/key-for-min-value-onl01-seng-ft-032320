5a85616e5854df65bd0b79a52ec9ef7bf9bad71e
Joshua Williams <jmw0426@hotmail.com>
2020-03-30 19:34:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0 
   return nil
   elsif name_hash[:blake] > name_hash[:ashley]
-    return ":ashley"
+    return :ashley
   elsif name_hash[:ashley] > name_hash[:adam]
-   return ":adam"
-  else name_hash[:adam] > name_hash[:blake]
-    return ":blake"
+   return :adam
+  elsif name_hash[:adam] > name_hash[:blake]
+    return :blake
  end
 end

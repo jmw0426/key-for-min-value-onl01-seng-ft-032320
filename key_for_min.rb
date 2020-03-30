66507ee32086762c0f81e5eaa263f455eaee539e
Joshua Williams <jmw0426@hotmail.com>
2020-03-30 20:39:21 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+  if name_hash.length == 0 
+  return nil
+  elsif name_hash[:blake] > name_hash[:ashley] && name_hash[:adam] > name_hash[:ashley] 
+    return :ashley
+  elsif name_hash[:ashley] > name_hash[:adam] && name_hash[:blake] > name_hash[:adam]
+   return :adam
+  else name_hash[:ashley] > name_hash[:blake]&& name_hash[:adam] > name_hash[:blake]
+    return :blake
+ end
 end

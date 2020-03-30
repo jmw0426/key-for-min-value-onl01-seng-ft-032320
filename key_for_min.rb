9af93dede979ec6055d67d0bbe5df593128532c9
Joshua Williams <jmw0426@hotmail.com>
2020-03-30 19:31:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,12 @@
 def key_for_min_value(name_hash)
   if name_hash.length == 0 
   return nil
-  elsif name_hash[:learn] > name_hash[:sad]
+  elsif name_hash[:blake] > name_hash[:ashley]
     return ":sad"
-  elsif name_hash[:sad] > name_hash[:happy]
+  elsif name_hash[:ashley] > name_hash[:happy]
    return ":happy"
-  else name_hash[:happy] > name_hash[:learn]
-    return ":learn"
+  else name_hash[:happy] > name_hash[:blake]
+    return ":blake"
  end
 end
+:blake => 500, :ashley => 2, :adam => 1}

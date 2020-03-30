d2600606d447eaf7ad56ff15d4bdbf37aedae208
Joshua Williams <jmw0426@hotmail.com>
2020-03-30 16:56:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key = hash.count{|a,b| a[1] <=> b[1]}.first
+  key = name_hashhash.count{|a,b| a[1] <=> b[1]}.first
   puts key
   
   if name_hash.length == 0
